cbb5415017fd5502ac5453e24ff087124ee3e53e
sileeee <tldms201@naver.com>
2024-11-07 15:33:07 +0400

    Top Banner Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from "react";
+import { HashRouter, Route, BrowserRouter } from "react-router-dom";
+import { Router, Switch } from "react-router-dom/cjs/react-router-dom.min";
+
+import BigBanner from "./routes/BigBanner";
+
+import "./App.css";
+import './styleguide.css';
+
 
 function App() {
   return (
-    <div>
-      <h1> hello </h1>
-    </div>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <div className="handubi">
+              <div className="div">
+                <BigBanner />
+                <div className="text-wrapper-7">Everything in one place</div>
+              </div>
+            </div>
+          </Route>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
